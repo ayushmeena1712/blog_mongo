4c2bca081ad4ee44e58135e2654e917727ca31e7
Ayush Meena <142333874+ayushmeena1712@users.noreply.github.com>
2024-11-21 10:01:52 +0530

    string is accepted

--- Frontend/src/axiosInstance.js
+++ Frontend/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import useAxiosPrivate from './usePrivateAxios';
-const BASE_URL = https://blog-mongo.onrender.com;
+const BASE_URL = "https://blog-mongo.onrender.com";
 
 export default axios.create({
     baseURL: BASE_URL
